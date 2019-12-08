69d66234bd8768c9e2a09e74edbbd5274b9b1bcc
m4nu56 <m4nu56@gmail.com>
2019-12-08 15:12:24 +0100

    feat: disable 404 catching api

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.get('*', (req, res) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+//app.use(function(req, res, next) {
+//    next(createError(404));
+//});
 
 // error handler
 app.use(function(err, req, res, next) {
