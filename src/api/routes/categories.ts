02d25e57c7fc8256f53ddd865ba5f731a673be5e
Emmanuel Balpe <e.balpe@dev1-0.com>
2019-12-25 21:18:54 +0100

    feat: categories route using new service

--- src/api/routes/categories.ts
+++ src/api/routes/categories.ts
@@ -1,20 +1,59 @@
-import { Request, Response, Router } from 'express';
-import {
-  createCategory,
-  deleteCategory,
-  getCategories,
-  getCategoryById,
-  updateCategory,
-} from '../../services/categoryQueries';
+import { NextFunction, Request, Response, Router } from 'express';
+import { Container } from 'typedi';
+import CategoryService from '../../services/CategoryService';
 
 const route = Router();
 
 export default (app: Router) => {
   app.use('/categories', route);
 
-  route.get('/', getCategories);
-  route.post('/', createCategory);
-  route.delete('/:id', deleteCategory);
-  route.patch('/:id', updateCategory);
-  route.get('/:id', getCategoryById);
+  route.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let categories = await Container.get(CategoryService).getCategories();
+      return res.send(categories);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
+  route.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const category = await Container.get(CategoryService).getCategoryById(req.params.id);
+      return res.status(200).send(category);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
+  route.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const category = await Container.get(CategoryService).create(req.body);
+      return res.status(201).send(category);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
+  route.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await Container.get(CategoryService).delete(req.params.id);
+      return res.status(200).send();
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
+  route.patch('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const category = await Container.get(CategoryService).update(req.params.id, req.body);
+      return res.status(200).send(category);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
 };
